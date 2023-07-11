bd3a32fd67769612f7d8b0f74dd967adb348633d
ryuki0414 <ryuki0414@gmail.com>
2023-07-11 23:41:48 +0900

    first commit

---
+++ kadai_index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>データ登録</title>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <style>div{padding: 10px;font-size:16px;}</style>
+</head>
+<body>
+
+<!-- Head[Start] -->
+<header>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+    <div class="navbar-header"><a class="navbar-brand" href="select.php">ブックマーク一覧</a></div>
+    </div>
+  </nav>
+</header>
+<!-- Head[End] -->
+
+<!-- Main[Start] -->
+<form method="post" action="kadai_insert.php">
+  <div class="jumbotron">
+   <fieldset>
+    <legend>お気に入りの本を登録</legend>
+     <label>本の名前：<input type="text" name="name"></label><br>
+     <label>本のURL：<input type="text" name="url"></label><br>
+     <label>コメント：<textArea name="comment" rows="4" cols="40"></textArea></label><br>
+     <input type="submit" value="送信">
+    </fieldset>
+  </div>
+</form>
+<!-- Main[End] -->
+
+
+</body>
+</html>
---
+++ kadai_insert.php
@@ -0,0 +1,38 @@
+<?php
+//1. POSTデータ取得
+//$name = filter_input( INPUT_GET, ","name" ); //こういうのもあるよ
+//$email = filter_input( INPUT_POST, "email" ); //こういうのもあるよ
+$name = $_POST['name']; //nameから取得
+$url = $_POST['url']; //emailから取得
+$comment = $_POST['comment']; //naiyouから取得
+
+
+
+
+//2. DB接続します
+try {
+  //Password:MAMP='root',XAMPP=''
+  $pdo = new PDO('mysql:dbname=ryuki0414_gs_db_kadai;charset=utf8;host=mysql57.ryuki0414.sakura.ne.jp','ryuki0414','ryuki0414_');
+} catch (PDOException $e) {
+  exit('DB Connection Error:'.$e->getMessage());
+}
+
+
+//３．データ登録SQL作成
+$stmt = $pdo->prepare("INSERT INTO gs_bm_table(name,url,comment,indate)VALUES(:name, :url, :comment, sysdate());");
+$stmt->bindValue(':name', $name, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':url', $url, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':comment', $comment, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$status = $stmt->execute();
+
+//４．データ登録処理後
+if($status==false){
+  //SQL実行時にエラーがある場合（エラーオブジェクト取得して表示）
+  $error = $stmt->errorInfo();
+  exit("SQL_ERROR:".$error[2]);
+}else{
+  //５．index.phpへリダイレクト
+  header("Location: kadai_index.php");
+  exit();
+}
+?>
