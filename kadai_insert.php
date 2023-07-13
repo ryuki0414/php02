923d97d37d2a4144f7b14fdddef0345a2b925fd4
ryuki0414 <ryuki0414@gmail.com>
2023-07-13 19:32:01 +0900

    230713

--- kadai_insert.php
+++ kadai_insert.php
@@ -12,13 +12,16 @@ $comment = $_POST['comment']; //naiyouから取得
 //2. DB接続します
 try {
   //Password:MAMP='root',XAMPP=''
-  $pdo = new PDO('mysql:dbname=ryuki0414_gs_db_kadai;charset=utf8;host=mysql57.ryuki0414.sakura.ne.jp','ryuki0414','ryuki0414_');
+  // $pdo = new PDO('mysql:dbname=ryuki0414_gs_db_kadai;charset=utf8;host=mysql57.ryuki0414.sakura.ne.jp','ryuki0414','ryuki0414_');
+  $pdo = new PDO('mysql:dbname=gs_db_kadai;charset=utf8;host=localhost','root','');
+
+
 } catch (PDOException $e) {
   exit('DB Connection Error:'.$e->getMessage());
 }
 
 
-//３．データ登録SQL作成
+//３．データ登録SQL
 $stmt = $pdo->prepare("INSERT INTO gs_bm_table(name,url,comment,indate)VALUES(:name, :url, :comment, sysdate());");
 $stmt->bindValue(':name', $name, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
 $stmt->bindValue(':url', $url, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
